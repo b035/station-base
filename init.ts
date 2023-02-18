62d59e63bd843e077f64ca2fa4931d2e9227f233
b035 <marlon@erler.cc>
2023-02-18 18:50:51 +0100

    added init script

---
+++ init.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import Fs from "fs/promises";
+
+import * as SDK from "@the-stations-project/sdk";
+
+async function init() {
+	//create directories
+	for (let path of [
+		"executables",
+		"registry",
+	]) {
+		await Fs.mkdir(path)
+			.catch(() => console.log(`failed to create directory "${path}".`));
+	}
+
+	//initialize registry
+	for (let path of [
+		"module_names",
+
+		"station_info",
+		"station_info/greetings",
+	]) {
+		(await SDK.Registry.mkdir(path))
+			.err(() => console.log(`failed to create registry directory "${path}".`));
+	}
+	for (let [path, content] of [
+		["startup_commands", ""],
+
+		["station_info/id", "my-station"],
+		["station_info/name", "My Station"],
+		["station_info/languages", "en"],
+		["station_info/greetings/en", "Welcome"],
+	]) {
+		(await SDK.Registry.write(path, content))
+			.err(() => console.log(`failed to write registry file "${path}".`));
+	}
+}
+
+init();
